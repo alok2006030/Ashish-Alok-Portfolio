44d9b3ab4aaeebfea39c6eef6d187950ce3fbfcb
Ashish Alok <ashishalok01@gmail.com>
2024-07-14 12:39:07 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-// Importing required components and styles
-import Navbar from "./components/Navbar/Navbar"; 
+import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
-import Skills from "./components/Skills/Skills";
+import Services from "./components/Services/Services";
 import "./App.css";
+import Experience from "./components/Experience/Experience";
+import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonial from "./components/Testimonials/Testimonial";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import { useContext } from "react"; // Importing useContext hook from React
-import { themeContext } from "./Context"; // Importing the themeContext from a custom context
-
-
-// App component function
+import { useContext } from "react";
+import { themeContext } from "./Context";
 function App() {
-  // Accessing theme state from the context using useContext hook
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-
-  // Return statement for rendering the App component
   return (
-    // Styling dark mode and light mode based on the state from the context
     <div
       className="App"
       style={{
-        background: darkMode ? "black" : "", // Apply black background when in dark mode
-        color: darkMode ? "white" : "", // Apply white text color when in dark mode
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
       }}
     >
-      {/* Rendering child components */}
-      <Navbar /> {/* Renders the Navbar component */}
-      <Intro /> {/* Renders the Intro component */}
-      <Skills /> {/* Renders the Skills component */}
-      <Portfolio /> {/* Renders the Portfolio component */}
-      <Contact /> {/* Renders the Contact component */}
-      <Footer /> {/* Renders the Footer component */}
+      <Navbar />
+      <Intro />
+      <Services />
+      <Experience />
+      <Works />
+      <Portfolio />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-// Importing React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Importing the main App component from './App'
 import App from './App';
-
-// Importing the ThemeProvider from './Context'
 import { ThemeProvider } from './Context';
 
-// Rendering the App component wrapped with the ThemeProvider to provide theme context to all descendants
 ReactDOM.render(
-  // Wrap the App component with the ThemeProvider
   <ThemeProvider>
     <App />
   </ThemeProvider>,
-  // Rendering the JSX content inside the 'root' element in the HTML file
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
